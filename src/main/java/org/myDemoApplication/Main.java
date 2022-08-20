4900e43210f6e4479cb55de7529539547205e82c
manshkumar109071 <manshkumar109071@gmail.com>
2022-08-20 18:58:29 +0530

    sec

--- src/main/java/org/myDemoApplication/Main.java
+++ src/main/java/org/myDemoApplication/Main.java
@@ -2,6 +2,6 @@ package org.myDemoApplication;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Manish Kumar!");
+        System.out.println("Hello Manish!");
     }
 }
